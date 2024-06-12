efd094f2959f80f4bcd48df3428f1171cdb6d492
周强 <1652025058@qq.com>
2024-06-12 11:58:50 +0800

    提交

---
+++ src/main/java/com/lc/concurrency/multithreading/ThreadTest1.java
@@ -0,0 +1,12 @@
+package com.lc.concurrency.multithreading;
+
+/**
+ * @author lc
+ * @date 2024/6/12
+ */
+public class ThreadTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
---
+++ src/test/java/com/lc/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lc.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
