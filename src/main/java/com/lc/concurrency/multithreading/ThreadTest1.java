58f30ecd8625544fd79a33b6f5bf58b2ba5bb607
liucheng <1475494912@qq.com>
2026-03-26 09:09:27 +0800

    提交

--- src/main/java/com/lc/concurrency/multithreading/ThreadTest1.java
+++ src/main/java/com/lc/concurrency/multithreading/ThreadTest1.java
@@ -7,7 +7,21 @@ package com.lc.concurrency.multithreading;
 public class ThreadTest1 {
 
     public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println(121313);
+        MyThread myThread = new MyThread();
+        Thread thread1 = new Thread(myThread, "Thread 1");
+        Thread thread2 = new Thread(myThread, "Thread 2");
+        Thread thread3 = new Thread(myThread, "Thread 3");
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
+    }
+
+
+class MyThread implements  Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+": is running");
+    }
+
 }
